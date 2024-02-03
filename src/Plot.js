7bd0ab273451397983e3c857be47031f6d0cf5f5
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-02-03 22:26:56 +0700

    fix: fix local scope error

--- src/Plot.js
+++ src/Plot.js
@@ -1,54 +1,54 @@
-import { Line } from '@antv/g2plot';
+import { Line } from "@antv/g2plot";
 //create line plot
 function Plot() {
   const data = [
-    { week: '1', position: -1500, score: 200 },
-    { week: '2', position: -1150, score: 450 },
-    { week: '3', position: -1000, score: 470 },
-    { week: '4', position: -900, score: 520 },
-    { week: '5', position: -1000, score: 520 },
-    { week: '6', position: -650, score: 580 },
-    { week: '7', position: -900, score: 590},
-    { week: '8', position: -1000, score: 600},
-    { week: '9', position: -800, score: 605},
-    { week: '10', position: -600, score: 640 },
-    { week: '11', position: -500, score: 645 },
-    { week: '12', position: -400, score: 700 },
-  ]
+    { week: "1", position: -1500, score: 200 },
+    { week: "2", position: -1150, score: 450 },
+    { week: "3", position: -1000, score: 470 },
+    { week: "4", position: -900, score: 520 },
+    { week: "5", position: -1000, score: 520 },
+    { week: "6", position: -650, score: 580 },
+    { week: "7", position: -900, score: 590 },
+    { week: "8", position: -1000, score: 600 },
+    { week: "9", position: -800, score: 605 },
+    { week: "10", position: -600, score: 640 },
+    { week: "11", position: -500, score: 645 },
+    { week: "12", position: -400, score: 700 },
+  ];
 
-  let newData = [{ week: '1', position: -1500, score: 200 }]
+  let newData = [{ week: "1", position: -1500, score: 200 }];
   // console.log(data);
   //config for plot
-  const line = new Line('plot', {
+  const line = new Line("plot", {
     data: newData,
     height: 300,
     width: 400,
-    renderer: 'svg',
-    padding: 'auto',
-    xField: 'week',
-    yField: 'position',
-    seriesField: 'score',
+    renderer: "svg",
+    padding: "auto",
+    xField: "week",
+    yField: "position",
+    seriesField: "score",
     lineStyle: {
-      shadowColor: 'black',
+      shadowColor: "black",
       shadowBlur: 10,
       shadowOffsetX: 5,
       shadowOffsetY: 5,
-      cursor: 'pointer',
+      cursor: "pointer",
     },
     xAxis: {
-      nice:true,
+      nice: true,
       title: {
-        text: 'Week',
+        text: "Week",
         position: "mid",
         style: {
           fontSize: 16,
         },
-      }
+      },
     },
     yAxis: {
-      nice:true,
+      nice: true,
       title: {
-        text: 'Position',
+        text: "Position",
         position: "mid",
         style: {
           fontSize: 16,
@@ -58,27 +58,27 @@ function Plot() {
 
     annotations: [
       {
-        type: 'regionFilter',
-        start: ['min', -700],
-        end: ['max', '0'],
-        color: 'gold',
+        type: "regionFilter",
+        start: ["min", -700],
+        end: ["max", "0"],
+        color: "gold",
       },
       {
-        type: 'text',
-        position: ['min', -800],
-        content: '70% border',
+        type: "text",
+        position: ["min", -800],
+        content: "70% border",
         offsetY: -4,
         style: {
-          textBaseline: 'top',
-          fill: 'gold',
+          textBaseline: "top",
+          fill: "gold",
         },
       },
       {
-        type: 'line',
-        start: ['min', -700],
-        end: ['max', -700],
+        type: "line",
+        start: ["min", -700],
+        end: ["max", -700],
         style: {
-          stroke: 'gold',
+          stroke: "gold",
           lineDash: [2, 2],
         },
       },
@@ -86,14 +86,16 @@ function Plot() {
     smooth: true,
     point: {},
   });
-  
-  //as herself
-  let deltaLocal;
-  let i = 0;
 
   //timer function to update data step by step
-  function setTimer (delay) {
+  function setTimer(delay) {
+    
+
+    let i = 0;
     let timer = setInterval(function run() {
+      //as herself
+      let deltaLocal = 0;
+      
       //select elements to display changes
       const DELTA = document.querySelector(".delta");
       const POSITION = document.querySelector(".position");
@@ -101,7 +103,7 @@ function Plot() {
       const UP_ARROW = document.querySelector(".up-arrow");
       const DOWN_ARROW = document.querySelector(".down-arrow");
       const MINUS = document.querySelector(".minus");
-      
+
       //update data and compare changes
       deltaLocal = data[i].position - newData[i].position;
       newData.push(data[i]);
@@ -109,9 +111,9 @@ function Plot() {
       //update plot
       line.changeData(newData);
 
-      POSITION.textContent = `${-newData[i].position}/1888`
-      SCORE.textContent = `${newData[i].score}/1373`
-      
+      POSITION.textContent = `${-newData[i].position}/1888`;
+      SCORE.textContent = `${newData[i].score}/1373`;
+
       //condition for delta arrows
       if (deltaLocal > 0) {
         DELTA.textContent = deltaLocal;
@@ -119,37 +121,37 @@ function Plot() {
         DOWN_ARROW.classList.remove("show");
         UP_ARROW.classList.add("show");
         DELTA.style.color = "green";
-      } else if (deltaLocal < 0){
+      } else if (deltaLocal < 0) {
         DELTA.textContent = deltaLocal;
         MINUS.classList.remove("show");
         UP_ARROW.classList.remove("show");
-        DOWN_ARROW.classList.add("show")
+        DOWN_ARROW.classList.add("show");
         DELTA.style.color = "red";
       } else {
         DELTA.textContent = "";
         MINUS.classList.add("show");
         UP_ARROW.classList.remove("show");
-        DOWN_ARROW.classList.remove("show")
+        DOWN_ARROW.classList.remove("show");
         DELTA.style.color = "gray";
       }
 
       // console.log('data', newData, data, "shift", deltaLocal);
       i += 1;
-      
+
       //loop it
       if (i === 12) {
         clearInterval(timer);
 
         i = 0;
-        newData =[]
-        newData = [{ week: '1', position: -1500, score: 200 }];
-        
-        setTimer (2000);
+        newData = [];
+        newData = [{ week: "1", position: -1500, score: 200 }];
+
+        setTimer(2000);
       }
     }, delay);
   }
   setTimer(2000);
 
-  return  line;
+  return line;
 }
 export default Plot;
